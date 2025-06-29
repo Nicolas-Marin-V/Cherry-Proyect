94e68713045038837d530d8df7458173e40c5db6
Nicolas <nimarinv@unal.edu.co>
2025-06-29 16:53:07 -0500

    Primer commit

---
+++ seiTasks/src/main/java/com/mycompany/seitasks/SeiTasks.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seitasks;
+
+/**
+ *
+ * @author Nicolas Marin
+ */
+public class SeiTasks {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
